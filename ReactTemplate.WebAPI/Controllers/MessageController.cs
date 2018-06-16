1c1cb25d6c452556f889d3bab8c15438f611e533
Vasyl <mu.1.chested.09@gmail.com>
2018-06-16 20:46:10 +0300

    react template

---
+++ ReactTemplate.WebAPI/Controllers/MessageController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ReactTemplate.WebAPI.Controllers
+{
+    public class MessageController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(
+                HttpStatusCode.OK,
+                "Works 1");
+        }
+
+        public HttpResponseMessage Post(string message)
+        {
+            if (message == null)
+            {
+                return Request.CreateErrorResponse(
+                    HttpStatusCode.BadRequest,
+                    "Invalid input");
+            }
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+
+        public HttpResponseMessage Post()
+        {
+            //if (message == null)
+            //{
+            //    return Request.CreateErrorResponse(
+            //        HttpStatusCode.BadRequest,
+            //        "Invalid input");
+            //}
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
